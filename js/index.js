f0a25ccbbfb004f210bec68ad6f3d00d8c0228dd
McLean Carpenter <carpemc@gmail.com>
2020-08-23 20:40:59 -0600

    final tweaks

--- js/index.js
+++ js/index.js
@@ -11,34 +11,32 @@ $(document).ready(function(){
           }
         }).done(function(user){
             $.ajax({
-                url:'https://api.github.com/users/'+username+'/repos?per_page=100',
+                url:'https://api.github.com/users/'+username+'/repos?q=per_page=100&commits>0',
                 data:{
                     client_id:'b7adc7a945e46b8b8e26',
                     client_secret:'e88cef00a224fd0ef6b9bfe9dbcc2effd6975024',
                     sort: 'created: asc'
                 }
-            })
-            
-        .done(function(repos){
-            //Append each user repository into the repos div
-            $.each(repos, function(index, repo){
-                $('#repos').append(`
-                    <div class="card">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <a class="repo" href="${repo.html_url}" target="_blank">${repo.name}</p>
-                            </div>
-                            <div class="col-md-3">
-                                <a class="commits" href="${repo.html_url}/commits/master" target="_blank">See Commits</a>
-                            </div>
-                            <div class="col-md-4">
-                                <p class="dt">${repo.updated_at}</p>
+            }).done(function(repos){
+                //Append each user repository into the repos div
+                $.each(repos, function(index, repo){
+                    $('#repos').append(`
+                        <div class="card">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <a class="repo" href="${repo.html_url}" target="_blank">${repo.name}</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <a class="commits" href="${repo.html_url}/commits/master" target="_blank">See Commits</a>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="dt">${repo.updated_at}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                `);
+                    `);
+                });
             });
-        });
           //Render user profile data to html
           $('#user_profile').html(`
             <div class="card border-primary mb-3" style="max-width: 100rem;">
@@ -91,16 +89,21 @@ $(document).ready(function(){
         })
         //Request for obtaining public members when searching for an organization sorted in ascending order
         .done(function(orgs){
+
             $.ajax({
                 url:'https://api.github.com/orgs/'+orgname+'/public_members',
                 data:{
                     client_id:'b7adc7a945e46b8b8e26',
                     client_secret:'e88cef00a224fd0ef6b9bfe9dbcc2effd6975024',
-                    sort: 'created: asc'
+                    dataType: 'jsonp',
+                    sort: 'created: asc',
+                    success: function(orgsdata) {
+                        console.log(orgs);
+                    }
                 }
             })
             
-        .done(function(members,users){
+        .done(function(members){
             //loop through each organization for members and append them in the orgs div
             $.each(members, function(index, members){
                 $('#orgs').append(`
